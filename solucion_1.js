49bce9ab3529f469ddaa43284b16cc5b29ac1ab2
karol <karoln.oficiall@gmail.com>
2025-10-04 17:03:38 -0500

    primer commit

---
+++ solucion_1.js
@@ -0,0 +1,8 @@
+let base = parseInt(prompt("Ingrese la base del rectángulo:"));
+let altura = parseInt(prompt("Ingrese la altura del rectángulo:"));
+let area = base * altura;
+let perimetro = (base + altura) * 2;
+alert(`El área del rectángulo es: ${area}`);
+alert(`El perímetro del rectángulo es: ${perimetro}`);
+console.log(`El área del rectángulo es: ${area}`);
+console.log(`El perímetro del rectángulo es: ${perimetro}`);
