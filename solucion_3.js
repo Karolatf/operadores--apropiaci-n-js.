535ceb4da0e365f46d4bfd0ac116db70c6f87bb2
karol <karoln.oficiall@gmail.com>
2025-10-04 19:16:42 -0500

    finalizado

---
+++ solucion_3.js
@@ -0,0 +1,7 @@
+let visitas = 100;
+    visitas += 25;
+    visitas -= 10;
+    visitas *= 2;
+    
+    alert(visitas);
+    console.log(visitas);
---
+++ solucion_4.js
@@ -0,0 +1,13 @@
+
+let operacion = (20+10) / 5+3*2-4* (20 + 10) / 5 + 3 * 2 - 4* (20+10) /5+3*2-4;
+    //primero resolvemos los parentesis//
+    operacion = 30 / 5 + 3 * 2 - 4 * 30 / 5 + 3 * 2 - 4 * 30 / 5 + 3 * 2 - 4;
+    //luego las multiplicaciones //
+    operacion = 30 / 5 + 6 - 120 / 5 + 6 - 120 / 5 + 6 - 4;
+    //luego las divisiones//
+    operacion = 6 + 6 - 24 + 6 - 24 + 6 - 4;
+    // luego las sumas y restas //
+    operacion = -28;
+
+    alert(operacion);
+    console.log(operacion);
